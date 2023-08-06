5ed1696c5b639b1b04ca5ee3a3bb04c49f1a238d
nanette0103 <139376417+nanette0103@users.noreply.github.com>
2023-08-06 12:57:47 +0900

    llll

---
+++ src/Section02/Variabel04.java
@@ -0,0 +1,15 @@
+package Section02;
+
+public class Variabel04 {
+	public static void main(String[] args) {
+		float iFloat = 10.1f;
+		double iDouble = 10.1d;
+		double iDouble2 = 10.1; // double형 d생략 가능
+		
+		System.out.println("float 형: " + iFloat);
+		System.out.println("double 형: " + iDouble);
+		System.out.println("double 형: " + iDouble2);
+		
+	}
+
+}
---
+++ src/Section02/Varoabel06.java
@@ -0,0 +1,43 @@
+package Section02;
+/*
+ * S. String 형 (문자열)
+ * 	문자열을 저장할 수 있다.
+ * 	창조형 변수
+ * 
+ * 선언방법
+ * 	String 변수형;
+ * 
+ * 
+ * 기본형 - boolean, char, byte, short, int, long, float, double
+ * 		실제 값을 저장하고있는 변수
+ * 창조형 - 기본형을 제외한 모든 개체(class 코드로 되어있다)
+ * 		주소 값을 저장하고 있다.
+ * 		객체에는 속성(값-변수),기능(매서드)
+ */
+
+public class Varoabel06 {
+	public static void main(String[] args) {
+		String str = "안녕하세요.";
+		System.out.println("str: " + str);
+		/*
+		 * 
+		 * h e l l o
+		 * 0 1 2 3 4  => 인덱스번호
+		 */
+		
+		String hello = "hello";
+		System.out.println("1번 인덱스: " + hello.charAt(1));
+		System.out.println("2번 인덱스 부터 4번 인덱스 전까지: " + hello.substring(2,4));
+		
+		String addr = "서울 특별시 서대문구 신촌";
+		boolean isContain = addr.contains("특별시");
+		
+		//if 조건문
+		if(isContain) {
+			System.out.println("특별시민 입니다.");
+		}else {
+			System.out.println("str; "+str);
+		}
+		}
+
+}
